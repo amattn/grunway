15739ae3349201f4e5b802b3c666379dcd6daedc
amattn <hidden@example.com>
2013-09-20 04:40:13 -0700

    nicer build and version functions

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package grunway
 
-func Version() int64 {
-	return 1
+const (
+	BUILD_NUMBER   = 1
+	VERSION_STRING = "0.1"
+)
+
+func Build() int64 {
+	return BUILD_NUMBER
+}
+func Version() string {
+	return VERSION_STRING
 }
