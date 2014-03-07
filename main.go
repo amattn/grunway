2162f41bd8dab20ab515a0bda3b5755ed5c4a47a
amattn <hidden@example.com>
2014-03-06 16:32:03 -0800

    cleanup start logs

--- main.go
+++ main.go
@@ -8,9 +8,7 @@ import (
 )
 
 func Start(routerPtr *Router, host string) error {
-	log.Println("Starting")
-	log.Println("go v:", runtime.Version())
-	log.Println("grunway v:", Version())
+	log.Printf("Starting Grunway Server (%v, grunway v%v(%v))", runtime.Version(), Version(), BuildNumber())
 
 	if routerPtr.AllRoutesCount() == 0 {
 		return fmt.Errorf("3621140790 Router has no valid routes defined")
@@ -18,7 +16,6 @@ func Start(routerPtr *Router, host string) error {
 
 	log.Println("All Routes:")
 	fmt.Print(routerPtr.AllRoutesSummary())
-	log.Println("End Routes")
 
 	log.Println("Listening from ", host)
 	err := http.ListenAndServe(host, routerPtr)
